5226f99069b15c95df843dfa9a2df8db11131035
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-03 21:33:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MediaAritmetica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digíte três números para ser feito a média aritmética sobre eles.");
+            Console.Write("Primeiro número:");
+            double one=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Segundo número:");
+            double two=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Terceiro número:");
+            double three=Convert.ToDouble(Console.ReadLine());
+            double media=one + two + three;
+            double media2=media / 3;
+            Console.Clear();
+            Console.WriteLine($"Média={media2}");
+            
+
+            
+        }
+    }
+}
